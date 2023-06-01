8d94c19aad4a6017dd674afdf564c916119fdf45
tharinm <tharindumadhushan864@gmail.com>
2023-06-01 13:19:06 +0530

    try to fix img loading issue

--- src/Layouts/Header/HerosectionLayout/HeroSection.js
+++ src/Layouts/Header/HerosectionLayout/HeroSection.js
@@ -1,27 +1,29 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import heroImage from "../../../assets/images/header.png";
-import blur from '../../../assets/images/blur.png'
+import blur from "../../../assets/images/blur.png";
 import "./HeroStyle.css";
 import HeroCard from "../../../components/HeaderSection/Herocard/HeroCard";
-import { LazyLoadImage } from "react-lazy-load-image-component";
+import LazyLoad from "react-lazy-load";
 
 export default function HeroSection() {
   return (
+    <LazyLoad>
     <div className="hero-section">
       <div className="background">
-        <LazyLoadImage
-          effect="blur"
+        
+        <img
           img
           src={heroImage}
           alt=""
           style={{ width: "100%" }}
-          placeholderSrc={blur}
+          // placeholderSrc={blur}
         />
       </div>
       <Row className="hero-cardm " style={{}}>
         <HeroCard />
       </Row>
-    </div>
+      </div>
+      </LazyLoad>
   );
 }
